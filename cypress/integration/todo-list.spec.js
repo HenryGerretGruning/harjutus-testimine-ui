b44ca227389701ad384f4ca1202097a9df962268
henrijeret <>
2021-05-06 21:56:32 +0000

    Preview

---
+++ cypress/integration/todo-list.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context('Todo List', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+
+    it('interact with list', () => {
+        cy.get('input')
+            .type('Lorem ipsum').should('have.value', 'Lorem ipsum')
+        
+        cy.get('button').click().then(() => {
+            cy.get('input').should('have.value', '')
+            cy.get('ul').find('li').should('have.length', 1)
+
+            cy.get('li').find('button').click().then(() => {
+                cy.get('ul').find('li').should('have.length', 0)
+            })
+        })
+    });
+})
+  
